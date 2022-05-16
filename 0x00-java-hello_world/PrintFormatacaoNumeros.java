81691e078436dc7b376bb0274e9fc43f7d9393ad
tc-anaalves <anaalves@ciandt.com>
2022-05-16 12:00:28 -0300

    Formatando numeros

---
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -0,0 +1,10 @@
+import java.util.Locale;
+public class PrintFormatacaoNumeros {
+    public static void main(String[] args) {
+    		Locale.setDefault(Locale.US);
+		float taxa = 0.2456f;
+		float valor = 7654.321f;
+		System.out.printf("Valor: R$ %,1.2f\n", valor);
+		System.out.printf("Taxa: %.2f%%\n", taxa);
+	}
+}
