cb1b81e4f0588844b67e8a8f2f2cf429fa7e3069
tc-anaalves <anaalves@ciandt.com>
2022-05-18 12:14:38 -0300

    Status do Personagem

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,48 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public int getSaudeAtual() {
+        return this.saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+
+        if (this.saudeAtual > 0) {
+            this.status = "vivo";
+        } else {
+            this.status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        if ((this.saudeAtual + quantidadeDeCura) > 100) {
+            this.setSaudeAtual(100);
+        } else {
+            this.setSaudeAtual(this.saudeAtual + quantidadeDeCura);
+        }
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        if (this.saudeAtual <= quantidadeDeDano) {
+            this.setSaudeAtual(0);
+        } else {
+            this.setSaudeAtual(this.saudeAtual - quantidadeDeDano);
+        }
+    }
+
+}
