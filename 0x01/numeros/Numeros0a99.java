958dc652d5cecdab02a81998eb2ce0f27d722fd1
tc-anaalves <anaalves@ciandt.com>
2022-05-23 10:25:43 -0300

    Numeros 0 a 99

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,12 @@
+package numeros;
+
+public class Numeros0a99 {
+    public static void main(String[] args) {
+
+        for (int n = 0; n < 99; n++) {
+            System.out.print(n + ", ");
+        }
+        System.out.println(99);
+        
+    }
+}
