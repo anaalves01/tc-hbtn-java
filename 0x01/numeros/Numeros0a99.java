8126a0e2a85b584f6208eb440561725c8be90ba5
tc-anaalves <anaalves@ciandt.com>
2022-05-23 10:32:50 -0300

    Numeros pares de 0 a 100

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -1,5 +1,3 @@
-package numeros;
-
 public class Numeros0a99 {
     public static void main(String[] args) {
 
@@ -7,6 +5,6 @@ public class Numeros0a99 {
             System.out.print(n + ", ");
         }
         System.out.println(99);
-        
+
     }
 }
---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,12 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+
+        for (int n = 0; n < 98; n++) {
+            if (n % 2 == 0) {
+                System.out.print(n + ", ");
+            }
+        }
+        System.out.println(98);
+
+    }
+}
