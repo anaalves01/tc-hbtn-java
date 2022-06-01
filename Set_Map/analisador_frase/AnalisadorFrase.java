87225b786ecf2affe1244fb08daddf2cc5084d29
tc-anaalves <anaalves@ciandt.com>
2022-06-01 09:01:49 -0300

    Contagem de Palavras

---
+++ Set_Map/analisador_frase/AnalisadorFrase.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TreeMap;
+
+public class AnalisadorFrase {
+
+    public static TreeMap<String, Integer> contagemPalavras(String frase) {
+        TreeMap<String, Integer> tree = new TreeMap<>();
+        ArrayList<String> array = new ArrayList<>();
+
+        frase = frase.replace("?", "");
+        frase = frase.replace("!", "");
+        frase = frase.replace(".", "");
+
+        String[] frases = frase.split(" ");
+        for (int i = 0; i < frases.length; i++) {
+            array.add(frases[i].toLowerCase());
+        }
+
+        Collections.sort(array);
+        for (int i = 0; i < array.size(); i++) {
+            String palavra = array.get(i);
+            tree.put(palavra, Collections.frequency(array, palavra));
+        }
+
+        return tree;
+    }
+    
+}
