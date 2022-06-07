ef545ea17bc1de47349466fee70b69d4f2e24d43
tc-anaalves <anaalves@ciandt.com>
2022-06-07 16:06:32 -0300

    Salario Funcionario

---
+++ Lambda/liquido/Funcionario.java
@@ -0,0 +1,19 @@
+public class Funcionario {
+
+    private double salario;
+
+    public Funcionario() { }
+
+    public Funcionario(double salario) {
+        setSalario(salario);
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+}
