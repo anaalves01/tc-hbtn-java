0cbe7f75417328735f6d66df313a62854a506353
tc-anaalves <anaalves@ciandt.com>
2022-06-07 16:06:18 -0300

    Produto MarkUp

---
+++ Lambda/markup/Produto.java
@@ -0,0 +1,49 @@
+import java.util.function.*;
+
+public class Produto {
+
+    private String nome;
+    private Double preco;
+    private Double percentualMarkUp = 0.1;
+
+    Supplier<Double> precoComMarkUp = () -> getPreco() + (getPreco() * getPercentualMarkUp());
+    Consumer<Double> atualizarMarkUp = (Double valor) -> setPercentualMarkUp(valor/100);
+
+    public Produto() { }
+
+    public Produto(Double preco, String nome) {
+        setPreco(preco);
+        setNome(nome);
+    }
+
+    public Produto(String nome, Double preco, Double percentualMarkUp) {
+        setNome(nome);
+        setPreco(preco);
+        setPercentualMarkUp(percentualMarkUp);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Double getPercentualMarkUp() {
+        return percentualMarkUp;
+    }
+
+    public void setPercentualMarkUp(Double percentualMarkUp) {
+        this.percentualMarkUp = percentualMarkUp;
+    }
+    
+}
