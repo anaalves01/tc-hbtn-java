3f0e1e4bff7ff8ae10ccce9652a59ee311b858dd
tc-anaalves <anaalves@ciandt.com>
2022-06-13 13:22:04 -0300

    Relatorio Produtos por preco minimo

---
+++ Streams_Collectors_Optional/preco_minimo/Produto.java
@@ -0,0 +1,90 @@
+public class Produto {
+    
+    private int codigo;
+    private String nome;
+    private CategoriaProduto categoria;
+    private double preco;
+
+    public Produto() {}
+
+    public Produto(int codigo, String nome, CategoriaProduto categoria, double preco) {
+        setCodigo(codigo);
+        setNome(nome);
+        setCategoria(categoria);
+        setPreco(preco);
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public CategoriaProduto getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(CategoriaProduto categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((categoria == null) ? 0 : categoria.hashCode());
+        result = prime * result + codigo;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(preco);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Produto other = (Produto) obj;
+        if (categoria != other.categoria)
+            return false;
+        if (codigo != other.codigo)
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (Double.doubleToLongBits(preco) != Double.doubleToLongBits(other.preco))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s R$ %.2f", getCodigo(), getNome(), getCategoria(), getPreco());
+    }
+
+}
